3b0f8191d7d4d41b2a06a1ffd4646565584d5701
Tan Phuoc <22521157@gm.uit.edu.vn>
2024-12-12 22:48:49 +0700

    adding login functionality

--- Login/login.js
+++ Login/login.js
@@ -1,18 +1,19 @@
-import { user } from '../user.js';
+import { user } from "../user.js";
 
 $(document).ready(function () {
-    $('form').submit(function (event) {
-        event.preventDefault();
+	$("form").submit(function (event) {
+		event.preventDefault();
 
-        const email = $('#floatingInput').val();
-        const password = $('#floatingPassword').val();
+		const email = $("#floatingInput").val();
+		const password = $("#floatingPassword").val();
 
-        if (email === user.email && password === user.password) {
-            localStorage.setItem('user', JSON.stringify(user));
+		if (email === user.email && password === user.password) {
+			localStorage.setItem("user", JSON.stringify(user));
+			localStorage.setItem("avatar", "../user-avatar.png"); // Update with the actual path to the user's avatar
 
-            window.location.href = '../Landing/landing.html';
-        } else {
-            alert('Incorrect email or password.');
-        }
-    });
+			window.location.href = "../Landing/landing.html";
+		} else {
+			alert("Incorrect email or password.");
+		}
+	});
 });
